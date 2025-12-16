6dd1b04a395f3869e9a919617561c3a2ce65ea52
Jaimie Bowen <jraebowen@gmail.com>
2025-12-16 14:31:37 -0800

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+//css import
+import "./App.css";
+
+//component import
+import Navigation from "../Navigation/Navigation";
+
+function App() {
+  return <div className="page"></div>;
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <nav className="nav">
+      <div className="nav__content"></div>
+    </nav>
+  );
+}
+
+export default Navigation;
